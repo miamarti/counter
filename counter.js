95942c492b8e453c167421535d20f982f879f947
Miller Augusto <miller@ciandt.com>
2017-07-27 19:47:45 -0300

    layout

--- counter.js
+++ counter.js
@@ -2,25 +2,25 @@ var count = 0,
     click = 0,
     timer;
 document.body.addEventListener('click', function () {
-  click++;
+    click++;
 
-  if (click === 3) {
-    click = 0;
-  }
-  if (timer) {
-    clearTimeout(timer);    
-  }
-
-  var timer = setTimeout(function () {
-    if (click === 1) {
-      count++;
-    } else if (click === 2 && count > 0) {
-      count--;
+    if (click === 3) {
+        click = 0;
     }
-    click = 0;
-    var s = "000000000" + count;
-    document.querySelector('.number').innerHTML = s.substr(s.length-4);
+    if (timer) {
+        clearTimeout(timer);
+    }
 
-    (new Audio('beep3.mp3')).play();
-  }, 500);
+    var timer = setTimeout(function () {
+        if (click === 1) {
+            count++;
+        } else if (click === 2 && count > 0) {
+            count--;
+        }
+        click = 0;
+        var s = "000000000" + count;
+        document.querySelector('.number').innerHTML = s.substr(s.length - 4);
+
+        (new Audio('beep3.mp3')).play();
+    }, 500);
 });
